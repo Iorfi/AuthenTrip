cdc2434db46c535436b14ae548dbd0774de1ecfe
Bautista Iorfida <bautistaiorfida@gmail.com>
2023-06-13 19:53:49 -0300

    juego

--- dictionary-post-starter/public/app.js
+++ dictionary-post-starter/public/app.js
@@ -24,6 +24,10 @@ class App {
   constructor() {
     this.fetch = new Fetch();
 
+    const comenzar = document.querySelector('#start-button');
+    this._juego = this._juego.bind(this);
+    comenzar.addEventListener('submit', this._juego);
+
     const searchForm = document.querySelector('#search');
     this._onSearch = this._onSearch.bind(this);
     searchForm.addEventListener('submit', this._onSearch);
@@ -35,8 +39,45 @@ class App {
     const setForm = document.querySelector('#set');
     this._onSet = this._onSet.bind(this);
     setForm.addEventListener('submit', this._onSet);
-    
   }
+  _juego(event) {    
+    event.preventDefault();
+
+    fetch('https://opentdb.com/api.php?amount=2&category=22&difficulty=medium&type=multiple')
+  .then(response => response.json())
+    .then(data => {
+      const triviaContainer = document.getElementById('trivia-questions');
+      triviaContainer.innerHTML = ''; // Limpiar las preguntas anteriores
+      data.results.forEach(question => {
+        const questionElement = document.createElement('div');
+        questionElement.classList.add('question');
+        questionElement.innerHTML = `
+          <h2>${question.question}</h2>
+          <ul class="options">
+            ${question.incorrect_answers.map(option => `<li><button>${option}</button></li>`).join('')}
+            <li><button>${question.correct_answer}</button></li>
+          </ul>
+        `;
+        triviaContainer.appendChild(questionElement);
+      });
+
+      const buttons = document.querySelectorAll('.options button');
+      buttons.forEach(button => {
+        button.addEventListener('click', (event) => {
+          const selectedOption = event.target.textContent;
+          const correctAnswer = event.target.parentNode.parentNode.querySelector('li:last-child button').textContent;
+          if (selectedOption === correctAnswer) {
+            alert('¡Correcto!');
+          } else {
+            alert('Incorrecto. La respuesta correcta es: ' + correctAnswer);
+          }
+        });
+      });
+    })
+    .catch(error => console.error('Error:', error));
+
+
+    };
   
   _onSet(event) {
     event.preventDefault();
